f7bb1353b5fb71fa29d5ff8a061f9fe7826d01d1
petrenko-do <petrenko-do@ukr.net>
2020-02-20 13:40:51 +0100

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,21 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
 
+    $('.product-slider__inner') .slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 3
+    });
 
-    
     var mixer = mixitup('.products__inner-box');
 
+   
 
 
 });
