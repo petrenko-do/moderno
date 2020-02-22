e4f9eca5fac0e1d6d9f2845645b4aaca522a8412
petrenko-do <petrenko-do@ukr.net>
2020-02-22 17:04:32 +0100

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -13,9 +13,18 @@ $(function(){
         slidesToScroll: 3
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
     var mixer = mixitup('.products__inner-box');
 
-   
+    
 
 
 });
