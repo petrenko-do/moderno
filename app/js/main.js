2ac0598688aea7858e8c70cb2a9f30037e2ac04a
petrenko-do <densamigos@ukr.net>
2021-10-31 16:55:18 +0100

    added advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 $(function () {
   
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+
+$('.product-slider__inner').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
+
   var mixer = mixitup('.products__inner-box');
 
   
 })
+
+
+
