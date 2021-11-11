bee4017246ae13f6b37de7d8a721628b5c204f13
petrenko-do <densamigos@ukr.net>
2021-11-11 21:36:07 +0100

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -6,25 +6,41 @@ $(function () {
     readOnly: true
   });
 
-$('.product-slider__inner').slick({
-  dots: true,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 4
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+   
+
+   $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
 });
 
-var mixer = mixitup('.products__inner-box');
-
-$(".js-range-slider").ionRangeSlider({
-  type: "double",
-  min: 0,
-  max: 1000,
-  from: 200,
-  to: 500,
-  grid: true
+$('.icon-th-list').on('click', function(){
+  $('.product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active');
 });
 
+$('.icon-th-large').on('click', function(){
+  $('.product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active');
+});
+
+
+
+
+mixer = mixitup('.products__inner-box');
 
 
 
